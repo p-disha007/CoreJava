e9eb6a1d4e6509120f1fd1ce5df3da6ef9f69e64
user <user@DESKTOP-ATEQATC>
2024-06-25 18:58:26 +0530

    first commit

---
+++ b96corejava/src/com/tnif/encapsulationprog/Encapsulation.java
@@ -0,0 +1,14 @@
+//Program to demonstrate encapsulation
+package com.tnif.encapsulationprog;
+
+public class Encapsulation {
+	int serialNum=10;//Datavariable
+	String name;
+	int age;
+	
+	void show()//
+	{
+		System.out.println("Serialnumber"+serialNum+"\t Name"+name +"\t Age:"+ age);
+	}
+
+}
---
+++ b96corejava/src/com/tnif/encapsulationprog/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tnif.encapsulationprog;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Encapsulation ob= new Encapsulation();// object creation
+		ob.name="suma";// calling the variables
+		ob.age=33;
+		ob.serialNum=22;
+		ob.show();
+
+	}
+
+}
